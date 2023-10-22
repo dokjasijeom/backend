d3e8233a3fbd73651277575ba0feb5751c035e1b
gyeonggeun oh <gyeong9768@gmail.com>
2023-10-22 22:57:53 +0900

    feat: 기본 미들웨어 사용 설정

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 func main() {
 	app := fiber.New()
+
+	app.Use(helmet.New())
+	app.Use(csrf.New())
+	app.Use(limiter.New())
+	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
