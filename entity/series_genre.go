e2345eed8c87b5401e03a293875c27c4f41a1dc3
gyeonggeun oh <gyeong9768@gmail.com>
2024-02-12 17:37:06 +0900

    refactor: 테이블 이름 지정

--- entity/series_genre.go
+++ entity/series_genre.go
@@ -4,3 +4,7 @@ type SeriesGenre struct {
 	SeriesId uint `gorm:"primaryKey;column:series_id;type:int(11);not null"`
 	GenreId  uint `gorm:"primaryKey;column:genre_id;type:int(11);not null"`
 }
+
+func (SeriesGenre) TableName() string {
+	return "series_genres"
+}
--- entity/series_publish_day.go
+++ entity/series_publish_day.go
@@ -4,3 +4,7 @@ type SeriesPublishDay struct {
 	SeriesId     uint `gorm:"primaryKey;column:series_id;type:int(11);not null"`
 	PublishDayId uint `gorm:"primaryKey;column:publish_day_id;type:int(11);not null"`
 }
+
+func (SeriesPublishDay) TableName() string {
+	return "series_publish_days"
+}
