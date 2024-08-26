75f239bdbd6775aa6b4658adad7f2229a1523937
gyeonggeun oh <gyeong9768@gmail.com>
2024-08-26 21:04:36 +0900

    fix: 백오피스 작품 목록에서 과하게 Preload 하지 않기

--- repository/impl/series_repository_impl.go
+++ repository/impl/series_repository_impl.go
@@ -528,37 +528,10 @@ func (seriesRepository *seriesRepositoryImpl) GetSeriesByHashId(ctx context.Cont
 func (seriesRepository *seriesRepositoryImpl) GetBackofficeAllSeries(ctx context.Context) ([]entity.Series, error) {
 	var seriesResult []entity.Series
 
-	err := seriesRepository.DB.WithContext(ctx).Model(&entity.Series{}).Preload("Genres").Preload("Publishers").Preload("PublishDays").Preload("SeriesAuthors.Person").Preload("Episodes").Find(&seriesResult)
+	err := seriesRepository.DB.WithContext(ctx).Model(&entity.Series{}).Preload("Genres").Find(&seriesResult)
 	if err.Error != nil {
 		exception.PanicLogging(err.Error)
 		return nil, err.Error
-	}
-
-	for i := range seriesResult {
-		if seriesResult[i].SeriesType == "webnovel" {
-			seriesResult[i].DisplayTags = "#웹소설 "
-		} else {
-			seriesResult[i].DisplayTags = "#웹툰 "
-		}
-
-		for genreI := range seriesResult[i].Genres {
-			seriesResult[i].DisplayTags += "#" + seriesResult[i].Genres[genreI].Name + " "
-		}
-		seriesResult[i].TotalEpisode = uint(len(seriesResult[i].Episodes))
-		seriesResult[i].DisplayTags = seriesResult[i].DisplayTags[:len(seriesResult[i].DisplayTags)-1]
-
-		// 작가 유형 반영해서 Authors 필드에 반영
-		seriesResult[i].Authors = make([]entity.Person, 0)
-		for _, sa := range seriesResult[i].SeriesAuthors {
-			sa.Person.PersonType = sa.PersonType
-			seriesResult[i].Authors = append(seriesResult[i].Authors, sa.Person)
-		}
-		// 제공자 정보를 Providers 필드에 반영
-		seriesResult[i].Providers = make([]entity.Provider, 0)
-		for _, sp := range seriesResult[i].SeriesProvider {
-			sp.Provider.Link = sp.Link
-			seriesResult[i].Providers = append(seriesResult[i].Providers, sp.Provider)
-		}
 	}
 
 	return seriesResult, nil
